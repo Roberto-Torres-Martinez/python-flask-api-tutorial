d4d6a73d14a21a34e9f682b9bca88cbdf7d29ae4
Roberto Torres Martínez <roberxiri@gmail.com>
2025-02-09 20:32:27 +0000

    API Flask ended

---
+++ src/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    todos.append(dict(request_body))
+    print("Incoming request with the following body", request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    if position < 0 or position >= len(todos):
+        return jsonify({"error": "Position not found"}), 404
+
+    del todos[position]
+    return jsonify(todos), 200
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
